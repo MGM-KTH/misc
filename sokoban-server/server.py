a0f1a0fb13ab0c0684f7693f45139fe0d5541bcd
glindste <gustaflindstedt@gmail.com>
2013-07-26 14:03:47 +0200

    no crash if board doesn't exist

--- sokoban-server/server.py
+++ sokoban-server/server.py
@@ -19,11 +19,14 @@ def send_board(filename, client):
     Opens a file containing a Sokoban level (board)
     and sends it to the connected client
     """
-    f = open('boards/' + board, 'r')
-    for line in f:
-        print(line)
-        client.send(line)
-    f.close()
+    try:
+        f = open('boards/' + board, 'r')
+        for line in f:
+            print(line)
+            client.send(line)
+        f.close()
+    except IOError:
+        client.send("\n")
 
 host = "localhost"
 port = 5555
